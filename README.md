fa085298f19b4e3eec46f1873cb0a27d7f4d7bcf
Lê Nguyên <moingaymotlinhkien@gmail.com>
2022-06-09 08:27:07 +0700

    lab03_1 first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#LCN113
+
+Nội dung LAB02_2:
+    1. Cài đặt bootstrap, reactstrap, reactpopper
+
+
+Thực hiện:
+    1. Cài đặt bootstrap, reactstrap, reactpopper
+        npm add bootstrap
+        npm add reactstrap
+        npm add react-popper
+    Kiểm tra trong file package.json xem đã add thêm 3 thư viện này vào chưa. Có rồi thì ok!
+    
+ 
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand className="left" href="/">LCN1996</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
